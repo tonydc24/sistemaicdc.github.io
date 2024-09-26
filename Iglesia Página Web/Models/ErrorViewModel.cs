31ca37f664eaac079f71e39015f4591d8dfc0240
tonydc24 <124815616+tonydc24@users.noreply.github.com>
2024-09-25 19:03:10 -0600

    Fix

---
+++ Iglesia Página Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Iglesia_Página_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
