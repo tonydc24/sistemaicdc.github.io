bbc7be2e99fddb7436821232251db3af49720a5f
lainnnxd <lianvillalta2003@gmail.com>
2024-10-08 22:02:27 -0600

    Login

--- Iglesia Página Web/Data/ApplicationDbContext.cs
+++ Iglesia Página Web/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Iglesia_Página_Web.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Iglesia_Página_Web.Data
-{
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+namespace Iglesia_Página_Web.Data;
 
-        public DbSet<User> Usuarios { get; set; }
-        public DbSet <Noticia> Noticias { get; set; }
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<User> Usuario { get; set; }
+    public DbSet <Noticia> Noticias { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=SistemaWeb;Trusted_Connection=True;TrustServerCertificate=True");
     }
 }
