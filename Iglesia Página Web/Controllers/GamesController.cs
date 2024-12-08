46d9d8b15268542150a30281423b2d4265d6bbe8
tonydc24 <124815616+tonydc24@users.noreply.github.com>
2024-12-08 17:12:49 -0600

    Hotfix

--- Iglesia Página Web/Controllers/GamesController.cs
+++ Iglesia Página Web/Controllers/GamesController.cs
@@ -10,52 +10,5 @@ namespace Iglesia_Página_Web.Controllers
         {
             return View();
         }
-        public IActionResult Details(int id)
-        {
-            return View();
-        }
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Game model)
-        {
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            return View(model);
-        }
-        public IActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(int id, Game model)
-        {
-            if (ModelState.IsValid)
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            return View(model);
-        }
-
-        public IActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            return RedirectToAction(nameof(Index));
-        }
     }
 }
